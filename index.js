445145e3186be2cedb95572e85def0295e70ccfe
birme <jonas.birme@eyevinn.se>
2020-11-23 14:33:40 +0100

    Catch exception thrown by the parser

--- index.js
+++ index.js
@@ -641,139 +641,143 @@ class HLSVod {
       let timelinePosition = 0;
 
       parser.on('m3u', m3u => {
-        if (!this.segmentsInitiated[bw]) {
-          let position = 0;
-          let nextSplicePosition = null;
-          let spliceIdx = 0;
+        try {
+          if (!this.segmentsInitiated[bw]) {
+            let position = 0;
+            let nextSplicePosition = null;
+            let spliceIdx = 0;
 
-          // Remove segments in the beginning if we have a start time offset
-          if (this.startTimeOffset != null) {
-            let remain = this.startTimeOffset;
-            while(remain > 0) {
-              const removed = m3u.items.PlaylistItem.shift();
-              if (!removed) {
-                remain = 0;
-              } else {
-                remain -= removed.properties.duration * 1000;
+            // Remove segments in the beginning if we have a start time offset
+            if (this.startTimeOffset != null) {
+              let remain = this.startTimeOffset;
+              while(remain > 0) {
+                const removed = m3u.items.PlaylistItem.shift();
+                if (!removed) {
+                  remain = 0;
+                } else {
+                  remain -= removed.properties.duration * 1000;
+                }
               }
             }
-          }
 
-          for (let i = 0; i < m3u.items.PlaylistItem.length; i++) {
+            for (let i = 0; i < m3u.items.PlaylistItem.length; i++) {
 
-            if (this.splices[spliceIdx]) {
-              nextSplicePosition = this.splices[spliceIdx].position;
-              spliceBw = this._getNearestBandwidthForSplice(this.splices[spliceIdx], bw);
-            } else {
-              nextSplicePosition = null;
-            }
+              if (this.splices[spliceIdx]) {
+                nextSplicePosition = this.splices[spliceIdx].position;
+                spliceBw = this._getNearestBandwidthForSplice(this.splices[spliceIdx], bw);
+              } else {
+                nextSplicePosition = null;
+              }
 
-            const playlistItem = m3u.items.PlaylistItem[i];
-            let segmentUri;
-            let baseUrl;
+              const playlistItem = m3u.items.PlaylistItem[i];
+              let segmentUri;
+              let baseUrl;
 
-            const m = mediaManifestUri.match('^(.*)/.*?$');
-            if (m) {
-              baseUrl = m[1] + '/';
-            }
+              const m = mediaManifestUri.match('^(.*)/.*?$');
+              if (m) {
+                baseUrl = m[1] + '/';
+              }
 
-            if (playlistItem.properties.uri.match('^http')) {
-              segmentUri = playlistItem.properties.uri;
-            } else {
-              segmentUri = url.resolve(baseUrl, playlistItem.properties.uri);
-            }
-            if (playlistItem.properties.discontinuity) {
-              this.segments[bw].push({
-                discontinuity: true
-              });
-            }
-            let diff = 0;
-            if (nextSplicePosition != null && position + playlistItem.properties.duration > nextSplicePosition) {
-              debug(`Inserting splice at ${bw}:${position} (${i})`);
-              diff = position - nextSplicePosition;
-              if (this.segments[bw].length > 0 && !this.segments[bw][this.segments[bw].length - 1].discontinuity) {
-                // Only insert discontinuity if this is not the first segment
-                debug(`Inserting discontinuity at ${bw}:${position} (${i}/${m3u.items.PlaylistItem.length})`);
+              if (playlistItem.properties.uri.match('^http')) {
+                segmentUri = playlistItem.properties.uri;
+              } else {
+                segmentUri = url.resolve(baseUrl, playlistItem.properties.uri);
+              }
+              if (playlistItem.properties.discontinuity) {
                 this.segments[bw].push({
                   discontinuity: true
                 });
               }
-              if (this.splices[spliceIdx].segments[spliceBw]) {
-                debug(`Inserting ${this.splices[spliceIdx].segments[spliceBw].length} ad segments`);
-                this.splices[spliceIdx].segments[spliceBw].forEach(v => {
-                  let q = {
-                    duration: v[0],
-                    uri: v[1],
-                    timelinePosition: this.timeOffset != null ? this.timeOffset + timelinePosition : null,
-                    discontinuity: false
-                  }
-
-                  this.segments[bw].push(q);
-                  position += q.duration;
-                  timelinePosition += (q.duration * 1000);
-                });
-                if (i != m3u.items.PlaylistItem.length - 1) {
-                  // Only insert discontinuity after ad segments if this break is not at the end
-                  // of the segment list
-                  debug(`Inserting discontinuity after ad segments`);
+              let diff = 0;
+              if (nextSplicePosition != null && position + playlistItem.properties.duration > nextSplicePosition) {
+                debug(`Inserting splice at ${bw}:${position} (${i})`);
+                diff = position - nextSplicePosition;
+                if (this.segments[bw].length > 0 && !this.segments[bw][this.segments[bw].length - 1].discontinuity) {
+                  // Only insert discontinuity if this is not the first segment
+                  debug(`Inserting discontinuity at ${bw}:${position} (${i}/${m3u.items.PlaylistItem.length})`);
                   this.segments[bw].push({
                     discontinuity: true
                   });
                 }
-              }
-              spliceIdx++;
-            }
-            // Next splice is back-to-back?
-            if (this.splices[spliceIdx]) {
-              verbose(`Next splice ${this.splices[spliceIdx].position} <= ${position}`);
-            }
-            if (this.splices[spliceIdx] && (this.splices[spliceIdx].position + diff) <= position) {
-              debug(`Next splice is back-to-back, not inserting new segment`);
-              this.splices[spliceIdx].position += diff;
-              this.segments[bw].pop(); // Remove extra disc
-              i--;
-            } else {
-              let assetData = playlistItem.get('assetdata');
-              let cueOut = playlistItem.get('cueout');
-              let cueIn = playlistItem.get('cuein');
-              let cueOutCont = playlistItem.get('cont-offset');
-              let duration = 0;
-              if (typeof cueOut !== 'undefined') {
-                duration = cueOut;
-              } else if (typeof cueOutCont !== 'undefined') {
-                duration = playlistItem.get('cont-dur');
-              }
-              let cue = (cueOut || cueIn || cueOutCont || assetData) ? {
-                out: (typeof cueOut !== 'undefined'),
-                cont: (typeof cueOutCont !== 'undefined') ? cueOutCont : null,
-                in: cueIn ? true : false,
-                duration: duration,
-                assetData: (typeof assetData !== 'undefined') ? assetData: null
-              } : null;
-              let q = {
-                duration: playlistItem.properties.duration,
-                uri: segmentUri,
-                timelinePosition: this.timeOffset != null ? this.timeOffset + timelinePosition : null,
-                cue: cue
-              }
-              if (this.segments[bw].length === 0) {
-                // Add daterange metadata if this is the first segment
-                if (this.rangeMetadata) {
-                  q['daterange'] = this.rangeMetadata;                  
-                }
-              }
-              this.segments[bw].push(q);
-              position += q.duration;
-              timelinePosition += q.duration * 1000;
-            }
-          }
+                if (this.splices[spliceIdx].segments[spliceBw]) {
+                  debug(`Inserting ${this.splices[spliceIdx].segments[spliceBw].length} ad segments`);
+                  this.splices[spliceIdx].segments[spliceBw].forEach(v => {
+                    let q = {
+                      duration: v[0],
+                      uri: v[1],
+                      timelinePosition: this.timeOffset != null ? this.timeOffset + timelinePosition : null,
+                      discontinuity: false
+                    }
 
-          this.targetDuration[bw] = Math.ceil(this.segments[bw].map(el => el && el.duration ? el.duration : 0).reduce((max, cur) => Math.max(max, cur), -Infinity));
-          this.segmentsInitiated[bw] = true;
-        } else {
-          debug(`Segments for ${bw} already initiated, skipping`);
+                    this.segments[bw].push(q);
+                    position += q.duration;
+                    timelinePosition += (q.duration * 1000);
+                  });
+                  if (i != m3u.items.PlaylistItem.length - 1) {
+                    // Only insert discontinuity after ad segments if this break is not at the end
+                    // of the segment list
+                    debug(`Inserting discontinuity after ad segments`);
+                    this.segments[bw].push({
+                      discontinuity: true
+                    });
+                  }
+                }
+                spliceIdx++;
+              }
+              // Next splice is back-to-back?
+              if (this.splices[spliceIdx]) {
+                verbose(`Next splice ${this.splices[spliceIdx].position} <= ${position}`);
+              }
+              if (this.splices[spliceIdx] && (this.splices[spliceIdx].position + diff) <= position) {
+                debug(`Next splice is back-to-back, not inserting new segment`);
+                this.splices[spliceIdx].position += diff;
+                this.segments[bw].pop(); // Remove extra disc
+                i--;
+              } else {
+                let assetData = playlistItem.get('assetdata');
+                let cueOut = playlistItem.get('cueout');
+                let cueIn = playlistItem.get('cuein');
+                let cueOutCont = playlistItem.get('cont-offset');
+                let duration = 0;
+                if (typeof cueOut !== 'undefined') {
+                  duration = cueOut;
+                } else if (typeof cueOutCont !== 'undefined') {
+                  duration = playlistItem.get('cont-dur');
+                }
+                let cue = (cueOut || cueIn || cueOutCont || assetData) ? {
+                  out: (typeof cueOut !== 'undefined'),
+                  cont: (typeof cueOutCont !== 'undefined') ? cueOutCont : null,
+                  in: cueIn ? true : false,
+                  duration: duration,
+                  assetData: (typeof assetData !== 'undefined') ? assetData: null
+                } : null;
+                let q = {
+                  duration: playlistItem.properties.duration,
+                  uri: segmentUri,
+                  timelinePosition: this.timeOffset != null ? this.timeOffset + timelinePosition : null,
+                  cue: cue
+                }
+                if (this.segments[bw].length === 0) {
+                  // Add daterange metadata if this is the first segment
+                  if (this.rangeMetadata) {
+                    q['daterange'] = this.rangeMetadata;                  
+                  }
+                }
+                this.segments[bw].push(q);
+                position += q.duration;
+                timelinePosition += q.duration * 1000;
+              }
+            }
+
+            this.targetDuration[bw] = Math.ceil(this.segments[bw].map(el => el && el.duration ? el.duration : 0).reduce((max, cur) => Math.max(max, cur), -Infinity));
+            this.segmentsInitiated[bw] = true;
+          } else {
+            debug(`Segments for ${bw} already initiated, skipping`);
+          }
+          resolve();
+        } catch (exc) {
+          reject(exc);
         }
-        resolve();
       });
 
       if (!_injectMediaManifest) {
@@ -800,36 +804,40 @@ class HLSVod {
       debug(`Audio manifest URI: ${audioManifestUri}`);
 
       parser.on('m3u', m3u => {
-        if (this.audioSegments[groupId]) {
+        try {
+          if (this.audioSegments[groupId]) {
 
-          for (let i = 0; i < m3u.items.PlaylistItem.length; i++) {
-            const playlistItem = m3u.items.PlaylistItem[i];
-            let segmentUri;
-            let baseUrl;
+            for (let i = 0; i < m3u.items.PlaylistItem.length; i++) {
+              const playlistItem = m3u.items.PlaylistItem[i];
+              let segmentUri;
+              let baseUrl;
 
-            const m = audioManifestUri.match('^(.*)/.*?$');
-            if (m) {
-              baseUrl = m[1] + '/';
+              const m = audioManifestUri.match('^(.*)/.*?$');
+              if (m) {
+                baseUrl = m[1] + '/';
+              }
+              if (playlistItem.properties.uri.match('^http')) {
+                segmentUri = playlistItem.properties.uri;
+              } else {
+                segmentUri = url.resolve(baseUrl, playlistItem.properties.uri);
+              }
+              if (playlistItem.properties.discontinuity) {
+                this.audioSegments[groupId].push({
+                  discontinuity: true
+                });
+              }
+              let q = {
+                duration: playlistItem.properties.duration,
+                uri: segmentUri
+              };
+              this.audioSegments[groupId].push(q);
             }
-            if (playlistItem.properties.uri.match('^http')) {
-              segmentUri = playlistItem.properties.uri;
-            } else {
-              segmentUri = url.resolve(baseUrl, playlistItem.properties.uri);
-            }
-            if (playlistItem.properties.discontinuity) {
-              this.audioSegments[groupId].push({
-                discontinuity: true
-              });
-            }
-            let q = {
-              duration: playlistItem.properties.duration,
-              uri: segmentUri
-            };
-            this.audioSegments[groupId].push(q);
+            this.targetAudioDuration[groupId] = Math.ceil(this.audioSegments[groupId].map(el => el ? el.duration : 0).reduce((max, cur) => Math.max(max, cur), -Infinity));
           }
-          this.targetAudioDuration[groupId] = Math.ceil(this.audioSegments[groupId].map(el => el ? el.duration : 0).reduce((max, cur) => Math.max(max, cur), -Infinity));
+          resolve();
+        } catch(exc) {
+          reject(exc);
         }
-        resolve();
       });
 
       if (!_injectAudioManifest) {
