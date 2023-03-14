06fefb5f19f1c100e92a09a5e5a7a6bd45334b6b
Jonas Birmé <jonas.birme@eyevinn.se>
2023-03-14 13:23:55 +0100

    fix: EXT-X-KEY need to be handled the same way as the init segment (#85)

--- spec/hlsvod_drm_spec.js
+++ spec/hlsvod_drm_spec.js
@@ -41,7 +41,11 @@ describe("HLSVod DRM standalone", () => {
       .then(() => {
         const seqSegments = mockVod.getLiveMediaSequenceSegments(0);
         expect(seqSegments["8065760"][2].discontinuity).toEqual(true);
-        expect(seqSegments["8065760"][3].keys).not.toBeUndefined();
+        expect(seqSegments["8065760"][0].keys).toBeUndefined();
+        expect(seqSegments["8065760"][1].keys).toBeUndefined();
+        for (let i = 3; i < seqSegments["8065760"].length - 1; i++) {
+          expect(seqSegments["8065760"][i].keys).not.toBeUndefined();
+        }
 
         let m3u8 = mockVod.getLiveMediaSequences(0, "8065760", 0);
         let lines = m3u8.split("\n");
@@ -49,12 +53,31 @@ describe("HLSVod DRM standalone", () => {
 
         const seqAudioSegments = mockVod.getLiveMediaSequenceAudioSegments("default-audio-group", "en", 0);
         expect(seqAudioSegments[2].discontinuity).toEqual(true);
-        expect(seqAudioSegments[3].keys).not.toBeUndefined();
-
+        expect(seqAudioSegments[0].keys).toBeUndefined();
+        expect(seqAudioSegments[1].keys).toBeUndefined();
+        for (let i = 3; i < seqAudioSegments.length - 1; i++) {
+          expect(seqAudioSegments[i].keys).not.toBeUndefined();
+        }
+        
         m3u8 = mockVod.getLiveMediaAudioSequences(0, "default-audio-group", "en", 0);
         lines = m3u8.split("\n");
         expect(lines[13]).toEqual('#EXT-X-KEY:METHOD=SAMPLE-AES-CTR,URI="data:text/plain;base64,AAAAPnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB4iFnNoYWthX2NlYzJmNjRhYTc4OTBhMTFI49yVmwY=",KEYID=0x67B30C86756F57C5A0A38A23AC8C9178,KEYFORMATVERSIONS="1",KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"');
 
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it("always provides KEY tags for all media sequences", (done) => {
+    mockVod = new HLSVod("http://mock.com/mock.m3u8");
+    mockVod
+      .load(mockMasterManifest, mockMediaManifest, mockAudioManifest)
+      .then(() => {
+        for (let i = 0; i < mockVod.getLiveMediaSequencesCount() - 1; i++) {
+          const seqSegments = mockVod.getLiveMediaSequenceSegments(i);
+        }
         done();
       })
       .catch((err) => {
@@ -106,13 +129,49 @@ describe("HLSVod DRM after another DRM VOD", () => {
       .then(() => {
         let m3u8 = mockVod2.getLiveMediaSequences(0, "8065760", 10);
         let lines = m3u8.split("\n");
-        expect(lines[21]).toEqual('#EXT-X-KEY:METHOD=SAMPLE-AES-CTR,URI="data:text/plain;base64,AAAAPnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB4iFnNoYWthX2NlYzJmNjRhYTc4OTBhMTFI49yVmwY=",KEYID=0x800AACAA522958AE888062B5695DB6BF,KEYFORMATVERSIONS="1",KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"');
+        expect(lines[7]).toEqual('#EXT-X-KEY:METHOD=SAMPLE-AES-CTR,URI="data:text/plain;base64,AAAAPnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB4iFnNoYWthX2NlYzJmNjRhYTc4OTBhMTFI49yVmwY=",KEYID=0x800AACAA522958AE888062B5695DB6BF,KEYFORMATVERSIONS="1",KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"');
+        expect(lines[14]).toEqual('#EXT-X-DISCONTINUITY');
+        expect(lines[15]).toEqual('#EXT-X-MAP:URI="http://mock.com/v-0576p-1400k-libx264-init.mp4"');
+        expect(lines[22]).toEqual('#EXT-X-KEY:METHOD=SAMPLE-AES-CTR,URI="data:text/plain;base64,AAAAPnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB4iFnNoYWthX2NlYzJmNjRhYTc4OTBhMTFI49yVmwY=",KEYID=0x800AACAA522958AE888062B5695DB6BF,KEYFORMATVERSIONS="1",KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"');
+
         m3u8 = mockVod2.getLiveMediaAudioSequences(0, "default-audio-group", "en", 10);
         lines = m3u8.split("\n");
-        expect(lines[21]).toEqual('#EXT-X-KEY:METHOD=SAMPLE-AES-CTR,URI="data:text/plain;base64,AAAAPnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB4iFnNoYWthX2NlYzJmNjRhYTc4OTBhMTFI49yVmwY=",KEYID=0x67B30C86756F57C5A0A38A23AC8C9178,KEYFORMATVERSIONS="1",KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"');
+        expect(lines[14]).toEqual('#EXT-X-DISCONTINUITY');
+        expect(lines[15]).toEqual('#EXT-X-MAP:URI="http://mock.com/a-eng-0384k-aac-6c-init.mp4"');
+        expect(lines[22]).toEqual('#EXT-X-KEY:METHOD=SAMPLE-AES-CTR,URI="data:text/plain;base64,AAAAPnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB4iFnNoYWthX2NlYzJmNjRhYTc4OTBhMTFI49yVmwY=",KEYID=0x67B30C86756F57C5A0A38A23AC8C9178,KEYFORMATVERSIONS="1",KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"');
 
         done();
-      });    
+      })
+      .catch((err) => {
+        done(err);
+      });
+  });
+
+  it("always has the correct EXT-KEY on top", (done) => {
+    mockVod = new HLSVod("http://mock.com/mock.m3u8");
+    mockVod2 = new HLSVod("http://mock.com/mock2.m3u8");
+    mockVod
+      .load(mockMasterManifest, mockMediaManifest, mockAudioManifest)
+      .then(() => {
+        return mockVod2.loadAfter(mockVod, mockMasterManifest, mockMediaManifest, mockAudioManifest);
+      })
+      .then(() => {
+        let m3u8;
+        for (let i = 0; i < 12; i++) {
+          m3u8 = mockVod2.getLiveMediaSequences(0, "8065760", i);
+          lines = m3u8.split("\n");
+          expect(lines[7]).toEqual('#EXT-X-KEY:METHOD=SAMPLE-AES-CTR,URI="data:text/plain;base64,AAAAPnBzc2gAAAAA7e+LqXnWSs6jyCfc1R0h7QAAAB4iFnNoYWthX2NlYzJmNjRhYTc4OTBhMTFI49yVmwY=",KEYID=0x800AACAA522958AE888062B5695DB6BF,KEYFORMATVERSIONS="1",KEYFORMAT="urn:uuid:edef8ba9-79d6-4ace-a3c8-27dcd51d21ed"');
+        }
+        for (let i = 13; i < 15; i++) {
+          m3u8 = mockVod2.getLiveMediaSequences(0, "8065760", i);
+          lines = m3u8.split("\n");
+          expect(lines[6]).toEqual('#EXT-X-MAP:URI="http://mock.com/v-0576p-1400k-libx264-init.mp4"');
+        }
+        done();
+      })
+      .catch((err) => {
+        done(err);
+      });
   });
 });
 
